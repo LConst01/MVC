7d7aca3f8c57cf58f80bdc31ada5afeafafe878d
LConst01 <219712487+LConst01@users.noreply.github.com>
2026-03-10 16:51:56 -0300

    Add files via upload

--- models/Produto.php
+++ models/Produto.php
@@ -1,14 +1,20 @@
 <?php
 //DEFINE O QUE O SISTEMA É !
 ///Model é responsável pelos dados
-class Produtos {
+class Produto {
     public static function todos() {
+        //public static - permite chamar o método todos diretamente na classe
+        //static: se precisa criar objeto parar usar na classe ou não
+
         return [
-            ['nome' => 'Teclado Mecânico', 'preco' => 250],
-            ['nome' => 'Mouse Gamer', 'preco' => 150],
+            ['nome' => 'Teclado Mecânico', 'preco' => 250], // "=>" operador de atribuição de array
+            ['nome' => 'Mouse Gamer', 'preco' => 150],  
             ['nome' => 'Monitor 144hz', 'preco' => 1200]
         ];
     }
 }
+// private 
+// protected
+// public
 
 ?>
---
+++ views/produto-lista.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Produtos Disponíveis</h1>
+    <ul>
+        <?php foreach ($produtos as $produto): ?>
+                <li><?php echo $produto['nome'] ?> - R$ <?php echo $produto['preco']; ?></li>
+
+        <?php endforeach;?>
+    </ul>
+</body>
+</html>
